8e3ed86e9a8736e5d1a6767215aaebc09d092157
Eric DS <eric.d.s.info@gmail.com>
2020-12-28 12:48:41 +0000

    Send order threaded

--- src/netcp.cpp
+++ src/netcp.cpp
@@ -14,7 +14,8 @@
 
 int protected_main(int argc, char *argv[]);
 std::vector<std::string> split_str(std::string str_to_split);
-// void send_function(/* --- */);
+void send_file(std::exception_ptr &eptr, std::string &filename);
+void send_operations(const std::string &filename);
 // void receive_function(/* --- */);
 
 int main(int argc, char *argv[])
@@ -49,8 +50,12 @@ int protected_main(int argc, char *argv[])
     /* CLI menu */
     std::string arg;
     std::string action;
-    std::string helpmsg;
-    std::cout << "-- NETCP CLI INTERFACE --\n";
+    const std::string helpmsg = "\n\n\t\tALLOWED COMMANDS:\n"
+                                "Send [filename] | send the given file to another socket\n"
+                                "Receive [folder] | preare the given folder to receive files\n"
+                                "           (if the folder doesn't exsist a new folder is created)\n"
+                                "Quit | End the process.";
+    std::cout << "\n\n-- NETCP CLI INTERFACE --\n";
     do
     {
         std::cout << "command: ";
@@ -64,6 +69,7 @@ int protected_main(int argc, char *argv[])
         {
             std::cout << helpmsg;
         }
+
         if (action == "Receive" && arg.empty())
         {
             std::cout << "\nReceive needs a directory to store the files, try again\n";
@@ -72,21 +78,35 @@ int protected_main(int argc, char *argv[])
         {
             std::cout << "\nA file must be specified as argument to send, try again\n";
         }
+
         if (action == "Receive" && !arg.empty())
         {
-            std::cout << "Receiving into " << arg << " directory";
-            // Thread here
-        }
-        if (action == "Send" && !arg.empty())
-        {
-            std::cout << "Sending " << arg << " file";
+            std::cout << "\nReceiving into " << arg << " directory";
             // Thread here
         }
 
-        if (action != "Quit")
+        if (action == "Send" && !arg.empty())
+        {
+            std::cout << "\nSending " << arg << " file";
+
+            std::exception_ptr send_eptr{};
+            std::thread send_th(&send_file, std::ref(send_eptr),
+                                std::ref(arg));
+
+            send_th.join();
+            if (send_eptr)
+            {
+                std::rethrow_exception(send_eptr);
+            }
+        }
+
+        if (action != "Quit" && action != "Clear")
         {
             std::cout << "\n\n\tPress enter to continue ...\n";
             std::getchar();
+        }
+        if (action == "Clear" || action == "Quit")
+        {
             system("clear");
         }
 
@@ -113,4 +133,67 @@ std::vector<std::string> split_str(std::string str_to_split)
         result = temp;
     }
     return result;
+}
+
+void send_file(std::exception_ptr &eptr, std::string &filename)
+{
+    try
+    {
+        send_operations(filename);
+    }
+    catch (...)
+    {
+        eptr = std::current_exception();
+    }
+}
+
+void send_operations(const std::string &filename)
+{
+
+    sockaddr_in local_sock_addr = make_ip_address(8081, "127.0.0.1");
+    sockaddr_in exter_sock_addr = make_ip_address(8080, "127.0.0.1");
+
+    std::string buffer_str;
+    //char *buffer_str[_1KB_];
+
+    Message file_msg;
+
+    File local_file(filename, O_RDWR);
+
+    file_msg.file_size = local_file.length();
+
+    Socket my_socket(local_sock_addr);
+
+    size_t i = 0;
+    while (static_cast<char *>(local_file.region())[i] != '\0')
+    {
+        file_msg.msg_id = i;
+        buffer_str.push_back(static_cast<char *>(local_file.region())[i]);
+
+        if (i == 0)
+        {
+            strcpy(file_msg.name.data(), filename.c_str());
+            my_socket.send_to(file_msg,
+                              file_msg.text.size() - 1,
+                              exter_sock_addr);
+        }
+
+        if ((buffer_str.size() >= _1KB_) ||
+            (static_cast<char *>(local_file.region())[i + 1] == '\0'))
+        {
+            buffer_str.copy(
+                file_msg.text.data(),
+                file_msg.text.size() - 1,
+                0);
+            my_socket.send_to(
+                file_msg,
+                buffer_str.size(),
+                exter_sock_addr);
+            buffer_str.resize(0);
+        }
+
+        i++;
+    }
+    file_msg.msg_id = -2;
+    my_socket.send_to(file_msg, sizeof(file_msg), exter_sock_addr);
 }
